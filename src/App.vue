eec9a332a786a956d465cc2ac59e098663ea46b0
Paul Stierle <paul@Pauls-Air.fritz.box>
2022-06-14 21:42:36 +0200

    added vueuse

--- src/App.vue
+++ src/App.vue
@@ -10,12 +10,3 @@ const test = async () => {
   console.log(data);
 };
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
-}
-</style>
